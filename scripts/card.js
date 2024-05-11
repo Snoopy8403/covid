1881f522005ffc8e0325dacfbcbe342cbb8017c0
Snoopy8403 <gomba.laszlo84@gmail.com>
2024-05-11 11:32:53 +0200

    mainEnd

--- scripts/card.js
+++ scripts/card.js
@@ -1,16 +1,23 @@
 import 'zizi-card';
 
 export const addCard = (covidData) => {
-    console.log(covidData);
-
     const cardContainer = document.getElementById('card-container');
-
     cardContainer.insertAdjacentHTML('afterbegin',`
         <zizi-card title="${covidData.country}">
             <div class="card-content">
-                <div>${covidData.capital_city}</div>
-                <div>${covidData.abbreviation}</div>
-             </div>    
+                <div>Continent: ${covidData.continent}</div>
+                <div>Location: ${covidData.location}, ${covidData.lat} - ${covidData.long}</div>    
+                <div>Elevation: ${covidData.elevation_in_meters}m, Area: ${covidData.sq_km_area}km²</div>
+                <div>Capital city: ${covidData.capital_city}</div>
+                <div>Abbreviation: ${covidData.abbreviation}</div>
+                <div>Population: ${covidData.population}</div>
+                <div>Life expectary: ${covidData.life_expectancy}</div>
+                <div>Confirmed: ${covidData.confirmed}</div>
+                <div>Deaths: ${covidData.deaths}</div>
+                <div>Isolated: ${covidData.iso}</div>
+                <div>Recovered: ${covidData.recovered}</div>
+             </div>
+             <div class="card-footer">Last updated: ${covidData.updated}</div>    
         </zizi-card>
     `);
 }
--- scripts/form.js
+++ scripts/form.js
@@ -5,6 +5,8 @@ import { loadCovidData } from "./covid";
 export const initForm = () => { 
     const form = document.getElementById('covid-form');
     const countryInput = document.getElementById('country-input');
+    const cardsContainer = document.getElementById('card-container');
+    let displayedCountry = [];
     const countryList = [
         'Hungary',
         'France', 
@@ -24,8 +26,29 @@ export const initForm = () => {
     form.addEventListener('submit', async e => {
         e.preventDefault();
         const country = document.getElementById('country-input').value;
-        const covidData =  await loadCovidData(country);
-        addCard(covidData);
-        console.log(covidData);
+        const message = document.getElementById('message');
+        cardsContainer.insertAdjacentHTML('afterbegin', '<div id="loading-indicator" class="lds-dual-ring"></div>');
+        try {
+            const covidData =  await loadCovidData(country);
+            let index = displayedCountry.indexOf(country);
+            if (index == -1) {
+                displayedCountry.push(country);
+                addCard(covidData);
+            } else {
+                message.style.display = 'block';
+                message.innerHTML = `
+                    <div class="info">This information has been downloaded!</div>
+                `;
+                setTimeout(() =>  message.style.display = 'none', 2000);
+            }
+        } catch (error) {
+            message.style.display = 'block';
+            message.innerHTML = `
+                <div class="error">${error}</div>
+            `;
+            setTimeout(() =>  message.style.display = 'none', 2000);
+        } finally {
+            cardsContainer.removeChild(document.getElementById('loading-indicator'));
+        }
     })
 }
